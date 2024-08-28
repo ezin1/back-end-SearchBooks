aaddec0af471d9f70da944a90e2eedf36f4cef65
ezin1 <93697880+ezin1@users.noreply.github.com>
2024-08-28 11:55:46 -0300

    add trust proxy

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,50 +1,52 @@
-import express, {Application, Request, Response} from 'express';
+import express, { Application, Request, Response } from 'express';
 import rateLimit from 'express-rate-limit';
 import routes from './routes';
 import cron from 'node-cron';
 import { startKafkaConsumer } from '../infra/kafka/kafka';
 export const setupApp = () => {
     const app: Application = express();
-    
-    app.use(express.json());
 
-        app.use(
+    app.use(express.json());
+    app.set('trust proxy', 1);
+
+    
+    app.use(
         rateLimit({
-            windowMs: 1 * 60 * 1000, 
-            max: 100, 
+            windowMs: 1 * 60 * 1000,
+            max: 100,
             message: {
-            response: 'error',
-            message: 'Too many requests, please try again later.',
-            data: {
-                status: 429,
-                error: 'Too many requests',
-            },
+                response: 'error',
+                message: 'Too many requests, please try again later.',
+                data: {
+                    status: 429,
+                    error: 'Too many requests',
+                },
             },
         })
-        );
+    );
 
-        // cron.schedule('*/1 * * * *', () => {
-        //     startKafkaConsumer(
-        //       ['localhost:19092'], 
-        //       'scraping-client',
-        //       'example_consumer', 
-        //       'scraping-topic' 
-        //     ).then(() => {
-        //       console.log('Kafka consumer started successfully.');
-        //     }).catch((error) => {
-        //       console.error('Error starting Kafka consumer:', error);
-        //     });
-        //   });
+    // cron.schedule('*/1 * * * *', () => {
+    //     startKafkaConsumer(
+    //       ['localhost:19092'], 
+    //       'scraping-client',
+    //       'example_consumer', 
+    //       'scraping-topic' 
+    //     ).then(() => {
+    //       console.log('Kafka consumer started successfully.');
+    //     }).catch((error) => {
+    //       console.error('Error starting Kafka consumer:', error);
+    //     });
+    //   });
 
-        app.use('/api', routes);
-
-        app.get('/', (request: Request, response: Response) => {
-            response.json({
-              response: 'successfull',
-              message: 'Hello World 🌍',
-              data: {},
-            });
-          }); 
+    app.use('/api', routes);
+  
+    app.get('/', (request: Request, response: Response) => {
+        response.json({
+            response: 'successfull',
+            message: 'Hello World 🌍',
+            data: {},
+        });
+    });
 
 
 
